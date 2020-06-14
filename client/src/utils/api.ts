a5c5564c4866a6488d24f9a1a7405558bd2e37c8
Jonatan del Valle <delvallejonatan@gmail.com>
2020-06-14 20:59:07 -0300

    fix format

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -21,16 +21,14 @@ const api = <Data = unknown>(endpointUrl: string, { body, ...customConfig }: Opt
     stringifiedBody = JSON.stringify(body)
   }
 
-  return window
-    .fetch(`${apiBaseUrl}/${endpointUrl}`, { ...config, body: stringifiedBody })
-    .then(async response => {
-      if (response.ok) {
-        const data = await response.json()
-        return data
-      } else {
-        return Promise.reject(response)
-      }
-    })
+  return window.fetch(`${apiBaseUrl}/${endpointUrl}`, { ...config, body: stringifiedBody }).then(async response => {
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    } else {
+      return Promise.reject(response)
+    }
+  })
 }
 
 export default api
