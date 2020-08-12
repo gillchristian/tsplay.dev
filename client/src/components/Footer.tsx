f39c8178357d71d7017b52d9651f89cbdad536c5
gillchristian <gillchristiang@gmail.com>
2020-08-12 22:03:09 +0200

    feat(footer): Add GitHub link

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { css, jsx } from '@emotion/core'
 import { Palette } from '../constants'
 
 const styles = {
-  container: css`
+  footer: css`
     height: 50px;
     width: 100vw;
     font-size: 14px;
@@ -14,6 +14,19 @@ const styles = {
     flex-direction: column;
     align-items: center;
     justify-content: center;
+  `,
+  container: css`
+    width: 100%;
+    max-width: 720px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
+  `,
+  footerSection: css`
+    width: 250px;
+    margin-bottom: 10px;
+    text-align: center;
   `,
   link: css`
     text-decoration: underline;
@@ -28,20 +41,32 @@ const styles = {
 
 const Footer: React.FC = () => {
   return (
-    <div css={styles.container}>
-      <div>
-        <span>by </span>
-        <a href="https://dvnahuel.website/" rel="noopener noreferrer" target="_blank" css={styles.link}>
-          dvnahuel
-        </a>
-        <span> / </span>
-        <a href="https://gillchristian.xyz/" rel="noopener noreferrer" target="_blank" css={styles.link}>
-          gillchristian
-        </a>
-        <span> / </span>
-        <a href="https://jonidelv.me/" rel="noopener noreferrer" target="_blank" css={styles.link}>
-          jonidelv
-        </a>
+    <div css={styles.footer}>
+      <div css={styles.container}>
+        <div css={styles.footerSection}>
+          <span>by </span>
+          <a href="https://dvnahuel.website/" rel="noopener noreferrer" target="_blank" css={styles.link}>
+            dvnahuel
+          </a>
+          <span> / </span>
+          <a href="https://gillchristian.xyz/" rel="noopener noreferrer" target="_blank" css={styles.link}>
+            gillchristian
+          </a>
+          <span> / </span>
+          <a href="https://jonidelv.me/" rel="noopener noreferrer" target="_blank" css={styles.link}>
+            jonidelv
+          </a>
+        </div>
+        <div css={styles.footerSection}>
+          <a
+            href="https://github.com/gillchristian/tsplay.dev"
+            rel="noopener noreferrer"
+            target="_blank"
+            css={styles.link}
+          >
+            tsplay.dev on GitHub
+          </a>
+        </div>
       </div>
     </div>
   )
